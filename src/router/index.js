89abc662e9d8c0af912d3dc7691d557074408aac
Ahmet Berke Karaman <berkekaraman99@hotmail.com>
2024-12-26 22:38:10 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: () => import("../views/SearchView.vue"),
+    },
+    {
+      path: "/library",
+      name: "library",
+      component: () => import("../views/LibraryView.vue"),
+    },
+  ],
+});
+
+export default router;
